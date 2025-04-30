4341872e030b36797e5fe1bbb9f97b14372fdbf5
Mihajatiana <hei.mihajatiana@gmail.com>
2025-04-30 15:14:26 +0300

    fix: add constraint for the date interval

--- src/main/resources/database/migration/create_table_season.sql
+++ src/main/resources/database/migration/create_table_season.sql
@@ -3,5 +3,6 @@ CREATE TABLE season
     season_id       UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     start_date      TIMESTAMP NOT NULL,
     end_date        TIMESTAMP NOT NULL,
-    championship_id UUID      NOT NULL REFERENCES championship (championship_id)
+    championship_id UUID      NOT NULL REFERENCES championship (championship_id),
+    CHECK (start_date < end_date)
 )
