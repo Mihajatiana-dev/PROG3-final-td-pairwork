3aa30cd71d8d64890a528eefe8769f3222160437
Mihajatiana <hei.mihajatiana@gmail.com>
2025-05-01 21:56:24 +0300

    fix: add unique constraint

--- src/main/resources/database/migration/2_create_table_club.sql
+++ src/main/resources/database/migration/2_create_table_club.sql
@@ -5,17 +5,6 @@ CREATE TABLE club
     acronym       VARCHAR(5)          NOT NULL,
     year_creation INTEGER             NOT NULL,
     stadium       VARCHAR(200)        NOT NULL,
-    coach_id      UUID REFERENCES coach (coach_id)
-);
-
---db ok
-CREATE TABLE club
-(
-    club_id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    club_name        VARCHAR(200) UNIQUE NOT NULL,
-    acronym          VARCHAR(5)          NOT NULL,
-    year_creation    INTEGER             NOT NULL,
-    stadium          VARCHAR(200)        NOT NULL,
-    coach_id         UUID REFERENCES coach (coach_id),
-    championship_id  UUID NOT NULL
+    coach_id      UUID REFERENCES coach (coach_id),
+    UNIQUE (club_id, coach_id)
 );
