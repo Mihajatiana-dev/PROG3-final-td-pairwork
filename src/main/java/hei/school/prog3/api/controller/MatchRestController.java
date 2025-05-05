0215fceaf36d2b6fde3eaf97eb9ddcadd382a783
Mihajatiana <hei.mihajatiana@gmail.com>
2025-05-05 09:51:18 +0300

    feat: add prepared controllers and some classes for match

--- src/main/java/hei/school/prog3/api/controller/MatchRestController.java
+++ src/main/java/hei/school/prog3/api/controller/MatchRestController.java
@@ -1,8 +1,12 @@
 package hei.school.prog3.api.controller;
 
+import hei.school.prog3.api.dto.request.AddGoal;
+import hei.school.prog3.api.dto.request.UpdateMatchStatus;
+import hei.school.prog3.model.Match;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +15,20 @@ public class MatchRestController {
     @PostMapping("/matchMaker/{seasonYear}")
     public void createMatch() {
 
+    }
+
+    @GetMapping("/matches/{seasonYear}")
+    public List<Match> getAllMatches() {
+        return null;
+    }
+
+    @PutMapping("/matches/{id}/status")
+    public Match updateMatchStatus(@PathVariable String id, @RequestBody UpdateMatchStatus status) {
+        return null;
+    }
+
+    @PostMapping("/matches/{id}/goals")
+    public Match addGoals(@PathVariable String id, @RequestBody List<AddGoal> goalsToAdd) {
+        return null;
     }
 }
